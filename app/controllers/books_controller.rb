a2a83e85361700ad29cc9106e6544608fe160dc4
Netlooo <130125070+Netlooo@users.noreply.github.com>
2023-06-24 22:12:13 +0000

    [add] viewfile

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to '/books'
+  end
+
+  def index
+    @books = Book.all
+    @user = User.find(params[:id])
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit,]
+  resources :books, only: [:new, :index, :show, :create,]
   root to: "homes#top"
+  devise_for :users
+  get "/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
