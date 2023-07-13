cf93c46c7f63e0e0a47a818b92564179d771ebff
Netlooo <130125070+Netlooo@users.noreply.github.com>
2023-07-13 08:18:34 +0000

    [add] test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
-    @user = User.find(params[:id])
+    @user = @book.user
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :create, :show, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
-  get "/about" => "homes#about"
+  get "home/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
